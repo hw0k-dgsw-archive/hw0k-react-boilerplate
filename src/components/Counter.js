df13d710810d5d62986a21d17e1bf955a4e0f076
남현욱 <37534735+hw0k@users.noreply.github.com>
2019-06-17 16:20:43 +0900

    [UPDATE] More declerative code

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,16 +3,17 @@ import { styled } from 'linaria/react';
 
 const Wrapper = styled.div`
   font-size: 2rem;
+  color: var(--oc-gray-8);
 `;
 
 const Counter = props => {
   return (
-    <Wrapper>
-      {props.number}
+    <div>
+      <Wrapper>{props.number}</Wrapper>
       <button onClick={() => props.increment(1)}>+</button>
       <button onClick={() => props.decrement(1)}>-</button>
       <button onClick={() => props.incrementAsync({delay: 1000, number: 1})}>1초 뒤 +</button>
-    </Wrapper>
+    </div>
   );
 };
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import CounterContainer from './CounterContainer';
 
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={CounterContainer} />
-      <Route path="/aa" component={() => (<div>Hello world!</div>)} />
-    </Switch>
+    <>
+      <Switch>
+        <Route exact path="/" component={() => (<div>Hello world!</div>)} />
+        <Route path="/counter" component={CounterContainer} />
+        <Route component={() => (<div>404</div>)} />
+      </Switch>
+      <Link to="/"><button>홈으로</button></Link>
+      <Link to="/counter"><button>카운터로</button></Link>
+    </>
   );
 };
 
